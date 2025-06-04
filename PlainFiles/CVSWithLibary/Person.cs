7a50e1498e51a7ade24bf51fa43f17727a689b31
valentinaM22 <valentinamoralesvelasquez@gmail.com>
2025-06-03 21:43:51 -0500

    :)

--- PlainFiles/CVSWithLibary/Person.cs
+++ PlainFiles/CVSWithLibary/Person.cs
@@ -12,8 +12,26 @@ public class Person
     public override string ToString()
     {
         return $"{Id}\t{FirstName} {LastName}\n\t" +
-            $"Phone: {Phone}\n\t" +
-            $"City: {City}\n\t" +
-            $"Balance: {Balance,20:C2}\n";
+               $"Phone: {Phone}\n\t" +
+               $"City: {City}\n\t" +
+               $"Balance: {Balance,20:C2}\n";
+    }
+
+    public static Person FromLine(string line)
+    {
+        var parts = line.Split(',');
+        return new Person
+        {
+            Id = int.Parse(parts[0]),
+            FirstName = parts[1],
+            LastName = parts[2],
+            Phone = parts[3],
+            City = parts[4],
+            Balance = decimal.Parse(parts[5])
+        };
+    }
+    public string ToLine()
+    {
+        return $"{Id},{FirstName},{LastName},{Phone},{City},{Balance}";
     }
 }
